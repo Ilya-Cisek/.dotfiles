3c522352791af2cc26bd7d64147f155b3bb80aee
Ilya-Cisek <sia@silentwings.ru>
2026-09-25 14:03:37 +0300

    nvim upd

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,9 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "goolord/alpha-nvim",
     event = "VimEnter",
     dependencies = { "nvim-tree/nvim-web-devicons" },
